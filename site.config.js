4e2602c5b41b15dbe77899ff229e2ff6aaebd691
SaJH <wogur981208@gmail.com>
2024-01-10 01:13:02 +0900

    Update config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Turtle SaJH",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "backend developer",
+    bio: "I am a developer who has a good technical curiosity but prioritizes making good products.    ",
+    email: "wogur981208@gmail.com",
+    linkedin: "",
+    github: "saJaeHyukc",
     instagram: "",
   },
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/saJaeHyukc/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Turtle SaJH",
+    description: "welcome to Turtle SaJH's blog",
   },
 
   // CONFIG configration (required)
